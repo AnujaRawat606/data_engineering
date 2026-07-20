3a3cc979d2351d930eb8d661c403fe168d5ae7e8
Anuja Rawat <anujarawat606@gmail.com>
2026-07-20 12:20:13 +0200

    Update and rename load_data.sql to load_customer_data.sql

---
+++ sql/load_customer_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers
+(customer_id, first_name, last_name, email, country)
+VALUES
+(1, 'Anna', 'Schmidt', 'anna@email.com', 'Germany'),
+(2, 'John', 'Smith', 'john@email.com', 'USA'),
+(3, 'Maria', 'Garcia', 'maria@email.com', 'Spain');
