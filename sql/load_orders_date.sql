b350b96f252fcbdd92396a9170549fe1fa622c81
Anuja Rawat <anujarawat606@gmail.com>
2026-07-20 12:20:30 +0200

    Add SQL script to load sample orders data

---
+++ sql/load_orders_date.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO orders
+(order_id, customer_id, product_id, quantity, order_date)
+VALUES
+(5001, 1, 101, 1, '2026-01-10'),
+(5002, 2, 102, 2, '2026-01-11'),
+(5003, 1, 103, 3, '2026-01-12');
+
