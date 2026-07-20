21c5c4545ac4283cc87b7da2108ac42e7439ed29
Anuja Rawat <anujarawat606@gmail.com>
2026-07-20 12:20:43 +0200

    Add SQL script to load product data

---
+++ sql/load_products_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO products
+(product_id, product_name, category, price)
+VALUES
+(101, 'Laptop', 'Electronics', 1200.00),
+(102, 'Headphones', 'Electronics', 150.00),
+(103, 'Coffee Mug', 'Home', 15.00);
